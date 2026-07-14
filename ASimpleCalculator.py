dee7749690c2d8822e930a5276921d5835128a8b
TheSuitedBear <callumsmith1315.business@gmail.com>
2026-07-14 02:33:25 +0100

    Started progress on A Simple Calculator

---
+++ ASimpleCalculator.py
@@ -0,0 +1,16 @@
+# A Simple Calculator
+
+import tkinter
+
+# Here needs to be a function for the actual calculations.
+def calculate():
+    pass
+
+
+window = tkinter.Tk()
+window.title("A Simple Calculator")
+window.geometry("600x450")
+window.configure(background="lightgray")
+
+
+window.mainloop()

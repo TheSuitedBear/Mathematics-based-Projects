15583752cc239558dfdd8b6f3d3d26c6be6a0e2d
Callum Smith <callumsmith1315.business@gmail.com>
2026-07-22 02:27:38 +0100

    Add initial implementation for Pi to the Nth digit calculator

---
+++ PiToTheNthDigit.py
@@ -0,0 +1,10 @@
+# Input a number and the program will return Pi with the inputted amount of digits
+
+from math import pi
+
+# I'll be honest I'm a bit stumped on how to actually use Pi here, maths version of Pi is only to a certain number of digits before its innacurate.
+def piDigits():
+    return
+
+# Input area for any integer number
+digits = int(input("Enter the amount of digits of Pi you would like: "))
